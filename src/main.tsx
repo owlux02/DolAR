1b0dfe49fa6dad98a2377a5c72a161727e369144
martínval11 <martinvaldez.dev@gmail.com>
2024-04-20 19:02:55 -0300

    pwa support

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,21 @@ import { Route, Switch } from 'wouter';
 import { App } from './app.tsx';
 import { Nav } from './components/nav/nav.tsx';
 import { OtherCurrencies } from './pages/otherCurrencies/page.tsx';
+import { Conversor } from './pages/conversor/page.tsx';
 
 import '@picocss/pico/css/pico.min.css';
 import './index.css';
-import { Conversor } from './pages/conversor/page.tsx';
+
+import { registerSW } from 'virtual:pwa-register';
+
+// add this to prompt for a refresh
+const updateSW = registerSW({
+	onNeedRefresh() {
+		if (confirm('New content available. Reload?')) {
+			updateSW(true);
+		}
+	},
+});
 
 render(
 	<>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,52 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
-})
+  plugins: [
+    preact(),
+    VitePWA({
+      // add this to cache all the imports
+      workbox: {
+        globPatterns: ["**/*"],
+        maximumFileSizeToCacheInBytes: 3000000,
+      },
+      // add this to cache all the
+      // static assets in the public folder
+      includeAssets: ["**/*"],
+      manifest: {
+        theme_color: "#13171F",
+        background_color: "#13171F",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        short_name: "DólAR",
+        description: "Revisa los precios del dolar u otras monedas de una forma más sencilla",
+        name: "DólAR",
+        icons: [
+          {
+            src: "/favicon192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/favicon256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/favicon384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/favicon512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
