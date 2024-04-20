7c0b2a95229d0fd7f84e39c77f63041c955d09c1
martínval11 <martinvaldez.dev@gmail.com>
2024-04-20 18:45:39 -0300

    icons

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,52 +1,67 @@
-import { useState } from "preact/hooks";
-import { Link } from "wouter";
-import styles from "./nav.module.css";
+import { useState } from 'preact/hooks';
+import { Link } from 'wouter';
+import styles from './nav.module.css';
 
 export function Nav() {
-  const [openMenu, setOpenMenu] = useState(false);
-  const { nav, links, title, linksMobile, menuMobileButton } = styles;
+	const [openMenu, setOpenMenu] = useState(false);
+	const { nav, links, title, linksMobile, menuMobileButton } = styles;
 
-  return (
-    <nav className={nav}>
-      <h1 className={title}>DólAR</h1>
+	return (
+		<nav className={nav}>
+			<h1 className={title}>DólAR</h1>
 
-      <div className={links}>
-        <Link href="/">Inicio</Link>
-        <Link href="/otrasDivisas">Otras divisas</Link>
-        <Link href="/conversor">Conversor</Link>
-        <a
-          href="https://github.com/martinval11/DolAR"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
-      </div>
+			<div className={links}>
+				<Link href="/">Inicio</Link>
+				<Link href="/otrasDivisas">Otras divisas</Link>
+				<Link href="/conversor">Conversor</Link>
+				<a href="https://github.com/martinval11/DolAR" target="_blank" rel="noopener noreferrer">
+					GitHub
+				</a>
+			</div>
 
-      <button onClick={() => setOpenMenu(!openMenu)} className={menuMobileButton}>Menu</button>
-      <dialog open={openMenu}>
-        <article>
-          <header>
-            <button aria-label="Close" rel="prev" onClick={() => setOpenMenu(false)}></button>
-            <p>
-              <strong>Menu de Navegación</strong>
-            </p>
-          </header>
+			<button onClick={() => setOpenMenu(!openMenu)} className={menuMobileButton}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="lucide lucide-menu"
+				>
+					<line x1="4" x2="20" y1="12" y2="12" />
+					<line x1="4" x2="20" y1="6" y2="6" />
+					<line x1="4" x2="20" y1="18" y2="18" />
+				</svg>
+			</button>
+			<dialog open={openMenu}>
+				<article>
+					<header>
+						<button aria-label="Close" rel="prev" onClick={() => setOpenMenu(false)}></button>
+						<p>
+							<strong>Menu de Navegación</strong>
+						</p>
+					</header>
 
-      <div className={linksMobile}>
-        <Link href="/" onClick={() => setOpenMenu(false)}>Inicio</Link>
-        <Link href="/otrasDivisas" onClick={() => setOpenMenu(false)}>Otras divisas</Link>
-        <Link href="/conversor" onClick={() => setOpenMenu(false)}>Conversor</Link>
-        <a
-          href="https://github.com/martinval11/DolAR"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
-      </div>
-        </article>
-      </dialog>
-    </nav>
-  );
+					<div className={linksMobile}>
+						<Link href="/" onClick={() => setOpenMenu(false)}>
+							Inicio
+						</Link>
+						<Link href="/otrasDivisas" onClick={() => setOpenMenu(false)}>
+							Otras divisas
+						</Link>
+						<Link href="/conversor" onClick={() => setOpenMenu(false)}>
+							Conversor
+						</Link>
+						<a href="https://github.com/martinval11/DolAR" target="_blank" rel="noopener noreferrer">
+							GitHub
+						</a>
+					</div>
+				</article>
+			</dialog>
+		</nav>
+	);
 }
--- src/pages/conversor/page.tsx
+++ src/pages/conversor/page.tsx
@@ -1,117 +1,167 @@
-import { useRef, useState } from 'preact/hooks';
-import type { priceAPIResponse } from '../../types';
+import { useRef, useState } from "preact/hooks";
+import type { priceAPIResponse } from "../../types";
 
-import styles from './conversor.module.css';
+import styles from "./conversor.module.css";
 
 export function Conversor() {
-	const [firstCurrency, setFirstCurrency] = useState('Oficial');
-	const [secondCurrency, setSecondCurrency] = useState('Peso Argentino');
-	const [output, setOutput] = useState(0);
+  const [firstCurrency, setFirstCurrency] = useState("Oficial");
+  const [secondCurrency, setSecondCurrency] = useState("Peso Argentino");
+  const [output, setOutput] = useState(0);
 
-	const valueToConvertRef = useRef<HTMLInputElement>(null);
+  const valueToConvertRef = useRef<HTMLInputElement>(null);
 
-	const { container, inputContainer, inputFlex, inputFieldBottom, form, outputContainer } = styles;
+  const {
+    container,
+    inputContainer,
+    inputFlex,
+    inputFieldBottom,
+    form,
+    outputContainer,
+    invertCurrenciesButton
+  } = styles;
 
-	const convertCurrencies = async (event: SubmitEvent) => {
-		event.preventDefault();
+  const convertCurrencies = async (event: SubmitEvent) => {
+    event.preventDefault();
 
-		const data = localStorage.getItem('data');
-		const otherCurrenciesData = localStorage.getItem('dataOtherCurrencies');
+    const data = localStorage.getItem("data");
+    const otherCurrenciesData = localStorage.getItem("dataOtherCurrencies");
 
-		if (data && otherCurrenciesData) {
-			const mergedData = JSON.parse(data).concat(JSON.parse(otherCurrenciesData)).concat({
-				moneda: 'ARS',
-				casa: 'Banco Central',
-				nombre: 'Peso Argentino',
-				venta: 1,
-				compra: 1,
-				fechaActualizacion: '',
-			});
+    if (data && otherCurrenciesData) {
+      const mergedData = JSON.parse(data)
+        .concat(JSON.parse(otherCurrenciesData))
+        .concat({
+          moneda: "ARS",
+          casa: "Banco Central",
+          nombre: "Peso Argentino",
+          venta: 1,
+          compra: 1,
+          fechaActualizacion: "",
+        });
 
-			const firstCurrencyData = mergedData.find((currency: priceAPIResponse) => currency.nombre === firstCurrency);
-			const secondCurrencyData = mergedData.find((currency: priceAPIResponse) => currency.nombre === secondCurrency);
+      const firstCurrencyData = mergedData.find(
+        (currency: priceAPIResponse) => currency.nombre === firstCurrency
+      );
+      const secondCurrencyData = mergedData.find(
+        (currency: priceAPIResponse) => currency.nombre === secondCurrency
+      );
 
-			if (firstCurrencyData && secondCurrencyData && valueToConvertRef.current) {
-				const inputValue = parseFloat(valueToConvertRef.current.value); // Convertimos el valor a número
-				if (!isNaN(inputValue)) {
-					const conversionRate = (firstCurrencyData.venta / secondCurrencyData.venta) * inputValue;
+      if (
+        firstCurrencyData &&
+        secondCurrencyData &&
+        valueToConvertRef.current
+      ) {
+        const inputValue = parseFloat(valueToConvertRef.current.value); // Convertimos el valor a número
+        if (!isNaN(inputValue)) {
+          const conversionRate =
+            (firstCurrencyData.venta / secondCurrencyData.venta) * inputValue;
 
-					setOutput(Number(conversionRate.toFixed(2)));
-				}
-			}
-		}
-	};
+          setOutput(Number(conversionRate.toFixed(2)));
+        }
+      }
+    }
+  };
 
-	const invertCurrencies = () => {
-		setFirstCurrency(secondCurrency);
-		setSecondCurrency(firstCurrency);
-	};
+  const invertCurrencies = () => {
+    setFirstCurrency(secondCurrency);
+    setSecondCurrency(firstCurrency);
+  };
 
-	return (
-		<main className={container}>
-			<form onSubmit={convertCurrencies} className={form}>
-				<div className={inputContainer}>
-					<div>
-						<label htmlFor="1st currency">Primera divisa</label>
-						<div className={inputFlex}>
-							<input type="text" placeholder="0.00" id="1st currency" ref={valueToConvertRef} required />
-							<select
-								name="1st currency"
-								id="1st currency"
-								onChange={(e) => {
-									const event = e.target as HTMLSelectElement;
-									setFirstCurrency(event.value);
-								}}
-								value={firstCurrency}
+  return (
+    <main className={container}>
+      <form onSubmit={convertCurrencies} className={form}>
+        <div className={inputContainer}>
+          <div>
+            <label htmlFor="1st currency">Primera divisa</label>
+            <div className={inputFlex}>
+              <input
+                type="text"
+                placeholder="0.00"
+                id="1st currency"
+                ref={valueToConvertRef}
+                required
+              />
+              <select
+                name="1st currency"
+                id="1st currency"
+                onChange={(e) => {
+                  const event = e.target as HTMLSelectElement;
+                  setFirstCurrency(event.value);
+                }}
+                value={firstCurrency}
                 className={inputFieldBottom}
-							>
-								<option value="Oficial" selected>
-									Dólar Oficial
-								</option>
-								<option value="Blue">Dólar Blue</option>
-								<option value="Contado con liquidación">Dólar Contado con Liquidación</option>
-								<option value="Mayorista">Dólar Mayorista</option>
-								<option value="Cripto">Dólar Cripto</option>
-								<option value="Tarjeta">Dólar Tarjeta</option>
-								<option value="Euro">Euro</option>
-								<option value="Peso Argentino">Peso Argentino</option>
-							</select>
-						</div>
-					</div>
+              >
+                <option value="Oficial" selected>
+                  Dólar Oficial
+                </option>
+                <option value="Blue">Dólar Blue</option>
+                <option value="Contado con liquidación">
+                  Dólar Contado con Liquidación
+                </option>
+                <option value="Mayorista">Dólar Mayorista</option>
+                <option value="Cripto">Dólar Cripto</option>
+                <option value="Tarjeta">Dólar Tarjeta</option>
+                <option value="Euro">Euro</option>
+                <option value="Peso Argentino">Peso Argentino</option>
+              </select>
+            </div>
+          </div>
 
-					<button onClick={invertCurrencies}>Invertir</button>
+          <button onClick={invertCurrencies} className={invertCurrenciesButton}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-arrow-right-left"
+            >
+              <path d="m16 3 4 4-4 4" />
+              <path d="M20 7H4" />
+              <path d="m8 21-4-4 4-4" />
+              <path d="M4 17h16" />
+            </svg>
+            <span>Invertir divisas</span>
+          </button>
 
-					<div>
-						<label htmlFor="2nd currency">Segunda divisa</label>
+          <div>
+            <label htmlFor="2nd currency">Segunda divisa</label>
 
-						<select
-							name="2nd currency"
-							id="2nd currency"
-							onChange={(e) => {
-								const event = e.target as HTMLSelectElement;
-								setSecondCurrency(event.value);
-							}}
-							value={secondCurrency}
+            <select
+              name="2nd currency"
+              id="2nd currency"
+              onChange={(e) => {
+                const event = e.target as HTMLSelectElement;
+                setSecondCurrency(event.value);
+              }}
+              value={secondCurrency}
               className={inputFieldBottom}
-						>
-							<option value="Peso Argentino" selected>
-								Peso Argentino
-							</option>
-							<option value="Oficial">Dólar Oficial</option>
-							<option value="Blue">Dólar Blue</option>
-							<option value="Contado con liquidación">Dólar Contado con Liquidación</option>
-							<option value="Mayorista">Dólar Mayorista</option>
-							<option value="Cripto">Dólar Cripto</option>
-							<option value="Tarjeta">Dólar Tarjeta</option>
-							<option value="Euro">Euro</option>
-						</select>
-					</div>
-				</div>
+            >
+              <option value="Peso Argentino" selected>
+                Peso Argentino
+              </option>
+              <option value="Oficial">Dólar Oficial</option>
+              <option value="Blue">Dólar Blue</option>
+              <option value="Contado con liquidación">
+                Dólar Contado con Liquidación
+              </option>
+              <option value="Mayorista">Dólar Mayorista</option>
+              <option value="Cripto">Dólar Cripto</option>
+              <option value="Tarjeta">Dólar Tarjeta</option>
+              <option value="Euro">Euro</option>
+            </select>
+          </div>
+        </div>
 
-				<button type="submit">Convertir</button>
-			</form>
+        <button type="submit">Convertir</button>
+      </form>
 
-			<div className={outputContainer}>$ {output.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}</div>
-		</main>
-	);
+      <div className={outputContainer}>
+        $ {output.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}
+      </div>
+    </main>
+  );
 }
